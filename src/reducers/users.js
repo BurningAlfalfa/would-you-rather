06e5dc6df38ec716002a033abc1ebe46824d24cf
BurningAlfalfa <acmonahan@gmail.com>
2022-05-16 16:58:27 -0500

    testing log in

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,12 @@
 import { RECEIVE_USERS } from "../actions/users";
 export default function users(state = {}, action) {
-  console.log("rec", action.users);
+  console.log(state, action);
   switch (action.type) {
     case RECEIVE_USERS:
       return {
         ...state,
         ...action.users,
+        users: action.payload,
       };
 
     default:
