21170fec68525333da9e3cc25d73cb2e90d916c1
BurningAlfalfa <acmonahan@gmail.com>
2023-09-25 00:34:19 -0500

    fixed css justify content

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { connect,useSelector, useDispatch} from "react-redux";
+import React, { useState} from "react";
+import { connect, } from "react-redux";
 import { addQuestion } from "../actions/questions";
 import { useNavigate } from "react-router-dom";
 import { Button, Input, Loader } from "semantic-ui-react";
 import { addQuestionToUser } from "../actions/users";
-import { logoutUser, setLastVisitedUrl } from "../actions/authedUser";
 
 const NewPoll = (props) => {
   const [optionOne, setOptionOne] = useState("");
   const [optionTwo, setOptionTwo] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const dispatch = useDispatch();
 
   const navigate = useNavigate(); // <-- Use the hook
  /* const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -55,8 +53,8 @@ const NewPoll = (props) => {
     >
       <h2 style={{ textAlign: "center" }}>Would you rather</h2>
       <Input
-        style={{ width: "fit-content", marginTop: 2 }}
-        justifyContent="center"
+        style={{ width: "fit-content", marginTop: 2 ,  justifyContent:"center"}}
+      
         placeholder="Enter option one..."
         value={optionOne}
         onChange={(e) => setOptionOne(e.target.value)}
@@ -68,8 +66,8 @@ const NewPoll = (props) => {
         onChange={(e) => setOptionTwo(e.target.value)}
       />
       <Button
-        style={{ marginTop: 20 }}
-        maxWidth={50}
+        style={{ marginTop: 20,        maxWidth:50
+}}
         onClick={handleSubmit}
         disabled={!optionOne || !optionTwo || loading}
       >
@@ -105,73 +103,15 @@ const mapDispatchToProps = (dispatch) => {
     },
   };
 };
-const mapStateToProps = (state, ownProps) => {
-  //const id = state.users.authedUser;
-  //const qid = state.questions.id;
+const mapStateToProps = (state, ) => {
   const { users } = state;
-  const { authedUser } = users;
   const { questions } = state;
   console.log({ questions, users });
   return {
-    // qid,
-    // id,
     authedUser: state.users.authedUser,
   };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewPoll);
 
-// import React, { Component } from "react";
-// import { connect } from "react-redux";
-// //import { handleAddTweet } from "../actions/tweets";
-// //import { Redirect } from "react-router-dom";
 
-// class NewPoll extends Component {
-//   state = {
-//     text: "",
-//     toHome: false,
-//   };
-//   handleChange = (e) => {
-//     const text = e.target.value;
-
-//     this.setState(() => ({
-//       text,
-//     }));
-//   };
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     const { text } = this.state;
-//     const { dispatch, id } = this.props;
-
-//     this.setState(() => ({
-//       text: "",
-//       toHome: id ? false : true,
-//     }));
-//   };
-//   render() {
-//     const { text, toHome } = this.state;
-//     if (toHome === true) {
-//       // return <Redirect to="/" />;
-//     }
-//     const tweetLeft = 280 - text.length;
-//     return (
-//       <div>
-//         <h3 className="center">Compose new Tweet</h3>
-//         <form className="new-tweet" onSubmit={this.handleSubmit}>
-//           <textarea
-//             placeholder="What's happening?"
-//             value={text}
-//             onChange={this.handleChange}
-//             className="textarea"
-//             maxLength={280}
-//           />
-//           {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
-//           <button className="button" type="submit" disabled={text === ""}>
-//             Submit
-//           </button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-// export default connect()(NewPoll);
